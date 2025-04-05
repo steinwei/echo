c37f9c60c300dfa975c6bbb960ee5aafc2cfaff0
Stein <weishijiestein@outlook.com>
2025-04-05 14:45:02 +0800

    feat: init

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <img :src="name" />
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Rate/Rate.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { defineProps, computed } from "vue"
+import Icon from "../Icon/Icon.vue";
+
+type RateProps = {
+  max: number;
+  icon: string;
+  readonly: boolean;
+  voidIcon: string;
+}
+
+const props = defineProps<RateProps>()
+
+function getRateStatus(score: number, index: number) {
+  if (score > index) {
+    return { status: 'full', score: 1 }
+  }
+  return { status: 'empty', score: 0}
+}
+
+const list = computed(() => {
+  return getRateStatus(props.max, props.icon)
+})
+
+const onClickItem = (index: number) => {}
+
+const updateRanges = () => {}
+
+const getScoreByMouse = () => {}
+</script>
+
+<template>
+  <div class="rate">
+    <div v-for="(item, index) in list">
+      <Icon :name="item.score > index ? icon : voidIcon" />
+    </div>
+  </div>
+</template>
